8183dcec0990efe98d7d4c0094dcd139d928af66
harpahlif <hhg33@hi.is>
2019-02-28 16:23:43 +0000

    Update Inngangur.py

--- Inngangur.py
+++ Inngangur.py
@@ -1,3 +1,4 @@
+from veljalit import Veljalit
 from Spurningaleikur import spurningaleikur
 
 #Segja velkominn, viltu spila
@@ -12,20 +13,14 @@ class inngangur:
 
 #Runnar veljalit - virkar ekki
 def main():
+    byrja = input('Viltu spila leikinn? Sláðu inn 1 fyrir já eða 0 fyrir nei\n')
+    #byrja = 1
     #Viltu velja lit?
-    val=1
-    if val==1:
-        print('hallo')
-        bord1= Veljalit()
-        bord1.spurningar_litur(bord1)
+    if byrja=='1':
+        litur= Veljalit()
+        litur.veljalit_litur(litur)
 
-#Runnar spurningaleik
-def main():
-    #þú ert á byrjunarreit
-    #Gerðu fval fyrir framhaldið
-    val=1
-    if val==1:
-        print('hallo')
+        #Viltu hefja leik? Já eða nei
         bord1= spurningaleikur()
         bord1.spurningar_bord1(bord1)
 
