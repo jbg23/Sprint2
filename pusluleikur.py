b64496954febecd1b395898631f176edc91e8b7b
Jóhanna Blöndahl <jbg23@hi.is>
2019-03-01 10:15:52 +0000

    PUSL

--- pusluleikur.py
+++ pusluleikur.py
@@ -15,7 +15,9 @@ svartur = (0, 0, 0)
 
 #Rammi á hvert púsl
 larettur_rammi = pygame.Surface((puslbreidd, 1))
+larettur_rammi.fill(svartur) #vitum ekki hvers vegna virkar ekki
 lodrettur_rammi = pygame.Surface((1, puslhaed))
+lodrettur_rammi.fill(svartur)
 
 #Setja myndaskrá inn í mynd og skipta í púsl
 mynd = pygame.image.load(myndaskra)
@@ -40,19 +42,87 @@ stada = {(dal, rad): (dal, rad)
 (tomurD, tomurR)= tomur
 
 #Byrjum leikinn og birtum upphaflega mynd
+
 pygame.init()
 display = pygame.display.set_mode(myndastaerd)
-pygame.display.set_caption("Hreyfa púsl")
+pygame.display.set_caption("Púslaðu Mikka!")
 display.blit(mynd, (0, 0))
 pygame.display.flip()
+"""while True:
+    event = pygame.event.poll()
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        sys.exit()
+    pygame.display.update()"""
 
 #Skipta á tóma púslinu og púsli (d,r)
 def skipti (d,r):
     global tomurD
     global tomurR
-    display.blit(pusl[stada[(d,r)]], (tomurD*puslbreidd, tomurR*puslhaed))
-    display.blit(pusl[tomur], (d*puslbreidd, r*puslhaed))
+    print('test1')
+    display.blit(pusl[stada[(d,r)]], tomurD*puslbreidd, tomurR*puslhaed)
+    print('test2')
+    #display.blit(pusl[tomur], (d*puslbreidd, r*puslhaed))
     stada[(tomurD, tomurR)]=stada[(d,r)]
     stada[(d,r)] = tomur
     (tomurD, tomurR) = (d,r)
     pygame.display.flip()
+
+#Rugla púslbitum
+def rugla():
+    global tomurD
+    global tomurR
+    sidastaR = 0
+    for i in range(75): #ATH 75?
+        pygame.time.delay(50)
+        while True:
+            #Veljum random átt til að færa púslin í.
+            r=random.randint(1,4)
+            if(sidastaR + r == 5):
+                continue
+            if r == 1 and (tomurD>0):
+                skipti(tomurD-1, tomurR) #vinstri
+            elif r == 4 and (tomurD<0):
+                skipti(tomurD+1, tomurR) #hægri
+            elif r == 2 and (tomurR > 0):
+                skipti(tomurD, tomurR-1) #upp
+            elif r == 3 and (tomurR < radir-1):
+                skipti(tomurD, tomurR+1) #niður
+            else:
+                sidastaR = r
+                continue
+            break #Búið að rugla púslunum
+
+def main():
+#Hreyfa púsl með mús
+    byrjun = True
+    synilausn = False
+    while True:
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            if byrjun: #Rugla eftir að ýtt er á mús í fyrsta sinn
+                rugla()
+                byrjun = False
+            elif event.dict['Hnappur'] == 1: #Ef ýtt á músina (vinstri), á púsl við hliðina á tómu púsli þá færist púslið.
+                mouse_pos = pygame.mouse.get_pos()
+                d = mouse_pos[0] / puslbreidd
+                r = mouse_pos[1] / puslhaed
+                if((abs(d-tomurD)==1 and r==tomurR)or (abs(r-tomurR) == 1 and d == tomurD)):
+                    skipti(d, r)
+            elif event.dict["Hnappur"] == 3:
+                #Hægri klikk, sýnir lausn myndar
+                vistud_mynd=display.copy()
+                display.blit(mynd, (0, 0))
+                pygame.display.flip()
+                synilausn = True
+        elif synilausn and (event.type == pygame.MOUSEBUTTONUP):
+            #Hætta að sýna lausnina
+            display.blit(vistud_mynd, (0, 0))
+            pygame.display.flip()
+            synilausn = False
+
+if __name__ == "__main__":
+    main()
